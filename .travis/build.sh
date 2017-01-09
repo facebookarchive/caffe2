f691e7b7096f0da35701d509862ac72c4172f744
Yangqing Jia <jiayq84@gmail.com>
2017-01-08 21:14:03 -0800

    build file minor cleans

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,10 +7,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 #************#
 # OS X build #
 #************#
-  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DUSE_OPENCV=off && make
+  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DUSE_OPENCV=off && make -j 4
 else
 #*************#
 # Linux build #
 #*************#
-  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON && make
+  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON && make -j 4
 fi
