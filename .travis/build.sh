fdc98fddc57c555006315b5ba457f629d78a0899
Yangqing Jia <jiayq84@gmail.com>
2017-01-08 21:40:31 -0800

    There seems to be a dependency error in cmake that prevents us from make -j 4. Reverting and will fix that in another day

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,10 +7,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 #************#
 # OS X build #
 #************#
-  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DUSE_OPENCV=off && make -j 4
+  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DUSE_OPENCV=off && make
 else
 #*************#
 # Linux build #
 #*************#
-  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON && make -j 4
+  cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON && make
 fi
