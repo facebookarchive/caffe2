b0275ec6ae209a0f33fdc4d912f60f1120d2ccc9
Yangqing Jia <jiayq@fb.com>
2017-02-13 11:35:57 -0800

    echo on to see why conda still asks.

--- scripts/appveyor/install.bat
+++ scripts/appveyor/install.bat
@@ -1,8 +1,6 @@
 :: Installation scripts for appveyor.
 
-@echo off 
-
-echo Downloading CUDA toolkit 8 ...
+@echo Downloading CUDA toolkit 8 ...
 
 appveyor DownloadFile ^
   https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe ^
@@ -11,7 +9,7 @@ appveyor Downloadfile ^
   http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-windows10-x64-v5.1.zip ^
   -FileName cudnn-8.0-windows10-x64-v5.1.zip
 
-echo Installing CUDA toolkit 8 ... 
+@echo Installing CUDA toolkit 8 ... 
 cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
 set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH% 
 :: TODO: we will still need to figure out how to install cudnn.
@@ -22,4 +20,4 @@ nvcc -V || exit /b
 :: Miniconda path for appveyor
 set PATH=C:\Miniconda-x64;C:\Miniconda-x64\Scripts;%PATH%
 :: Install numpy
-conda install numpy
+conda install -y numpy
