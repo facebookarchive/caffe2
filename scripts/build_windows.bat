86f4ea8e42c2f911f15aa1742b0afdb9e37f4f82
Yangqing Jia <jiayq@fb.com>
2017-02-13 10:57:38 -0800

    oh windows

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -8,7 +8,7 @@
 
 SET ORIGINAL_DIR=%cd%
 SET CAFFE2_ROOT=%~dp0%..
-if not exist %CAFFE2_ROOT%\build_host_protoc\bin\protoc.exe %CAFFE2_ROOT%\scripts\build_host_protoc.bat
+if not exist %CAFFE2_ROOT%\build_host_protoc\bin\protoc.exe call %CAFFE2_ROOT%\scripts\build_host_protoc.bat
 
 if not exist %CAFFE2_ROOT%\build mkdir %CAFFE2_ROOT%\build
 cd %CAFFE2_ROOT%\build
