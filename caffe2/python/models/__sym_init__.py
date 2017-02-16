bc3e2113323a74b54219da47ef7511d585151d5f
Bram Wasti <bwasti@fb.com>
2017-02-15 16:20:31 -0800

    Added downloader

---
+++ caffe2/python/models/__sym_init__.py
@@ -0,0 +1,12 @@
+import os
+from caffe2.proto import caffe2_pb2
+
+def _parseFile(filename):
+    out_net = caffe2_pb2.NetDef()
+    dir_path = os.path.dirname(__file__) # TODO(bwasti): more robust handler here.
+    with open('{dir_path}/{filename}'.format(dir_path=dir_path, filename=filename), 'rb') as f:
+        out_net.ParseFromString(f.read())
+    return out_net
+
+init_net = _parseFile('init_net.pb')
+predict_net = _parseFile('predict_net.pb')
