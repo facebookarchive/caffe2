d29122250bcfec64388a44252a0800a88d8795c2
ezineo <550498711@qq.com>
2017-02-21 16:40:49 +0800

    Update caffe_translator.py
    fix conflict

--- caffe2/python/caffe_translator.py
+++ caffe2/python/caffe_translator.py
@@ -325,18 +325,20 @@ def TranslatePool(layer, pretrained_blobs, is_test):
         caffe_op = BaseTranslate(layer, "AveragePool")
     _TranslateStridePadKernelHelper(param, caffe_op)
     AddArgument(caffe_op, "order", "NCHW")
-    if param.HasField('torch_pooling') and param.torch_pooling:
-         # In the Facebook port of Caffe, a torch_pooling field was added to		
-         # map the pooling computation of Torch. Essentially, it uses		
-         #   floor((height + 2 * padding - kernel) / stride) + 1		
-         # instead of		
-         #   ceil((height + 2 * padding - kernel) / stride) + 1		
-         # which is Caffe's version.		
-         # Torch pooling is actually the same as Caffe2 pooling, so we don't		
-         # need to do anything.		
-         pass		
-    else:		
-        AddArgument(caffe_op, "legacy_pad",		
+    try:
+        # In the Facebook port of Caffe, a torch_pooling field was added to
+        # map the pooling computation of Torch. Essentially, it uses
+        #   floor((height + 2 * padding - kernel) / stride) + 1
+        # instead of
+        #   ceil((height + 2 * padding - kernel) / stride) + 1
+        # which is Caffe's version.
+        # Torch pooling is actually the same as Caffe2 pooling, so we don't
+        # need to do anything.
+        is_torch_pooling = param.torch_pooling
+    except AttributeError:
+        is_torch_pooling = False
+    if not is_torch_pooling:
+        AddArgument(caffe_op, "legacy_pad",
                     caffe2_legacy_pb2.CAFFE_LEGACY_POOLING)
     if param.global_pooling:
         AddArgument(caffe_op, "global_pooling", 1)
