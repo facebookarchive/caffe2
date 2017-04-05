e5cac708354987502ea7e35d21c48c44c5deb1b3
Dongsheng Fang <550498711@qq.com>
2017-04-05 11:57:03 +0800

    Update windows_cpu_supports.h

--- caffe2/utils/windows_cpu_supports.h
+++ caffe2/utils/windows_cpu_supports.h
@@ -2,16 +2,16 @@
 #include <intrin.h>	
 inline bool __builtin_cpu_supports(const char * avx2)
 {																					
-	bool avxSupported = false;														
+	bool avx2Supported = false;														
 	int cpuInfo[4];																	
 	__cpuid(cpuInfo, 1);															
 	bool osUsesXSAVE_XRSTORE = cpuInfo[2] & (1 << 27) || false;						
-	bool cpuAVXSuport = cpuInfo[2] & (1 << 28) || false;							
-	if (osUsesXSAVE_XRSTORE && cpuAVXSuport)										
+	bool cpuAVX2Suport = cpuInfo[2] & (1 << 28) || false;							
+	if (osUsesXSAVE_XRSTORE && cpuAVX2Suport)										
 	{																				
 		unsigned long long xcrFeatureMask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);	
-		avxSupported = (xcrFeatureMask & 0x6) == 0x6;								
+		avx2Supported = (xcrFeatureMask & 0x6) == 0x6;								
 	}																				
-	return avxSupported;															
+	return avx2Supported;															
 }
 #endif
