a02f2035225acbed8e1a34c065f5d3f54e9935ce
Davin Wang <davin.wang@live.com>
2017-07-28 17:52:47 +0800

    fix #983 by remove unsupported archs
    `resize_op.cu` line63 leverages `__ldg` feature of CUDA, which requires compute_35 as minimum requirement.

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -1,7 +1,7 @@
 # Known NVIDIA GPU achitectures Caffe2 can be compiled for.
 # This list will be used for CUDA_ARCH_NAME = All option
-set(Caffe2_known_gpu_archs "20 21(20) 30 35 50 52 60 61")
-set(Caffe2_known_gpu_archs7 "20 21(20) 30 35 50 52")
+set(Caffe2_known_gpu_archs "35 50 52 60 61")
+set(Caffe2_known_gpu_archs7 "35 50 52")
 
 ################################################################################################
 # A function for automatic detection of GPUs installed  (if autodetection is enabled)
