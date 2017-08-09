fix the index calculation error.
Summary:
Fix the index calculation N and D which is reviewed by Maratyszcza.
set the atol and rtol value of test case in leaky_relu and softmax
to 1e-4, which is same as relu testcase

caffe2/contrib/nnpack/nnpack_ops_test.py
@@ -169,8 +169,8 @@ class NNPackOpsTest(hu.HypothesisTestCase):
         np.testing.assert_allclose(
             outputs[""],
             outputs["NNPACK"],
-            atol=1e-5,
-            rtol=1e-5)
+            atol=1e-4,
+            rtol=1e-4)
 
     @given(size=st.sampled_from([6, 8]),
            input_channels=st.integers(1, 8),
@@ -195,8 +195,8 @@ class NNPackOpsTest(hu.HypothesisTestCase):
         np.testing.assert_allclose(
             outputs[""],
             outputs["NNPACK"],
-            atol=1e-9,
-            rtol=1e-9)
+            atol=1e-4,
+            rtol=1e-4)
 
     @settings(timeout=3600)
     @unittest.skipIf(not os.environ.get("CAFFE2_BENCHMARK"), "Benchmark")