a498429a85b4d9e0bba179ba6fc82d9c3e36ed83
Yangqing Jia <jiayq84@gmail.com>
2017-09-25 21:07:11 -0700

    fix clang-lint

--- caffe2/core/common.h
+++ caffe2/core/common.h
@@ -122,7 +122,8 @@ private:                                                                       \
 #if __cplusplus >= 201402L ||                                              \
     (defined __cpp_lib_make_unique && __cpp_lib_make_unique >= 201304L) || \
     (defined(_MSC_VER) && _MSC_VER >= 1900)
-/* using override */ using std::make_unique;
+/* using override */
+using std::make_unique;
 #else
 
 template<typename T, typename... Args>
