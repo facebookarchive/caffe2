9450722c457d7331d70bca689466f76499f045f4
Fei Sun <feisun@fb.com>
2017-09-29 14:31:20 -0700

    Remove benchmark harness. The harness cannot be in the same repository of the Caffe2 since the harness needs to check out different versions of the caffe2 and compare the result. It is an issue if the harness is also changed.

--- caffe2/share/contrib/benchmark_harness/utils/subprocess_with_logger.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-from utils.custom_logger import getLogger
-
-def processRun(*args):
-    getLogger().info("Running: %s", ' '.join(*args))
-    return subprocess.check_output(*args)
