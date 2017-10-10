f1e2cdd2b813b411113e43c4ffd6324567e3a735
Fei Sun <feisun@fb.com>
2017-10-10 12:50:00 -0700

    Include init.h

--- caffe2/share/contrib/observers/net_observer_reporter_print.cc
+++ caffe2/share/contrib/observers/net_observer_reporter_print.cc
@@ -1,6 +1,8 @@
 #include "caffe2/share/contrib/observers/net_observer_reporter_print.h"
 #include "caffe2/share/contrib/observers/observer_config.h"
 
+#include "caffe2/core/init.h"
+
 namespace caffe2 {
 
 namespace {
