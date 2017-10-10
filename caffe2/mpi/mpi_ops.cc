facebookarchive/caffe2

commit e16871d87d06f3ae1adfc90bd43410c00cc4a330

Add linter for enforcing caffe operator documentation
Summary: Add check that every time we register a caffe operator to CPU or GPU that documentation is added for the particular operator.

Reviewed By: dzhulgakov

Differential Revision: D5443110

fbshipit-source-id: 3793c3d29bea1228078cb30bdf8243ac0ab90664

caffe2/mpi/mpi_ops.cc
@@ -1,7 +1,14 @@
 #include "caffe2/mpi/mpi_ops.h"
 
 namespace caffe2 {
-namespace {
+
+OPERATOR_SCHEMA(MPICreateCommonWorld);
+OPERATOR_SCHEMA(MPIBroadcast);
+OPERATOR_SCHEMA(MPIReduce);
+OPERATOR_SCHEMA(MPIAllgather);
+OPERATOR_SCHEMA(MPIAllreduce);
+OPERATOR_SCHEMA(MPISendTensor);
+OPERATOR_SCHEMA(MPIReceiveTensor);
 
 REGISTER_CPU_OPERATOR(MPICreateCommonWorld, MPICreateCommonWorldOp<CPUContext>);
 REGISTER_CPU_OPERATOR(MPIBroadcast, MPIBroadcastOp<CPUContext>);
@@ -11,5 +18,4 @@ REGISTER_CPU_OPERATOR(MPIAllreduce, MPIAllreduceOp<float, CPUContext>);
 REGISTER_CPU_OPERATOR(MPISendTensor, MPISendTensorOp<CPUContext>);
 REGISTER_CPU_OPERATOR(MPIReceiveTensor, MPIReceiveTensorOp<CPUContext>);
 
-}  // namespace
 }  // namespace caffe2

commit fffb87ecf6af8473f7e9dd58a55ec2bbe174f297

Fix broken MPI tests
Broken since e16871d87d06f3ae1adfc90bd43410c00cc4a330

caffe2/mpi/mpi_ops.cc
@@ -18,11 +18,23 @@
 
 namespace caffe2 {
 
-OPERATOR_SCHEMA(MPICreateCommonWorld);
-OPERATOR_SCHEMA(MPIBroadcast);
-OPERATOR_SCHEMA(MPIReduce);
-OPERATOR_SCHEMA(MPIAllgather);
-OPERATOR_SCHEMA(MPIAllreduce);
+OPERATOR_SCHEMA(MPICreateCommonWorld)
+  .NumInputs(0)
+  .NumOutputs(1);
+OPERATOR_SCHEMA(MPIBroadcast)
+  .NumInputs(2)
+  .NumOutputs(1)
+  .EnforceInplace({{1, 0}});
+OPERATOR_SCHEMA(MPIReduce)
+  .NumInputs(2)
+  .NumOutputs(1);
+OPERATOR_SCHEMA(MPIAllgather)
+  .NumInputs(2)
+  .NumOutputs(1);
+OPERATOR_SCHEMA(MPIAllreduce)
+  .NumInputs(2)
+  .NumOutputs(1)
+  .AllowInplace({{1, 0}});
 OPERATOR_SCHEMA(MPISendTensor);
 OPERATOR_SCHEMA(MPIReceiveTensor);
 