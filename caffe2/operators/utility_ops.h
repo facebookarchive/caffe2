4629ba96d5bfbbe407297ca0f2e6e85113c68504
Junjie Bai <bai@in.tum.de>
2017-10-12 11:37:48 -0700

    Add tests

--- caffe2/operators/utility_ops.h
+++ caffe2/operators/utility_ops.h
@@ -230,10 +230,8 @@ class FlattenOp : public Operator<Context> {
   bool RunOnDevice() override {
     auto& input = Input(0);
     auto* output = Output(0);
-    CAFFE_ENFORCE_GT(
-        input.dims().size(),
-        axis_,
-        "The rank of the tensor must be greater than axis.");
+    CAFFE_ENFORCE_GE(
+        input.dims().size(), axis_, "The rank of the tensor must be >= axis.");
     output->Resize(input.size_to_dim(axis_), input.size_from_dim(axis_));
     context_.template CopyItems<Context, Context>(
         input.meta(),
