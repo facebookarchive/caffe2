2193f17ff2ba8cc0cfa21148a229989b706f8b2c
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-12-01 18:44:24 +0000

    Use versioned Python binary

--- docker/jenkins/common/install_python.sh
+++ docker/jenkins/common/install_python.sh
@@ -13,11 +13,13 @@ case "$PYTHON_VERSION" in
     apt-get install -y --no-install-recommends \
             python-dev \
             python-setuptools
+    PYTHON=python2
   ;;
   3*)
     apt-get install -y --no-install-recommends \
             python3-dev \
             python3-setuptools
+    PYTHON=python3
   ;;
   *)
     echo "Invalid PYTHON_VERSION..."
@@ -32,7 +34,7 @@ esac
 curl -O https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz
 tar zxf pip-9.0.1.tar.gz
 pushd pip-9.0.1
-python setup.py install
+"$PYTHON" setup.py install
 popd
 rm -rf pip-9.0.1*
 
