948eb9220c994804187e52e9c2121dcd7e30fd4c
Dmytro Dzhulgakov <dzhulgakov@fb.com>
2017-12-02 15:18:12 -0800

    fp16 version of PadImage and add fix a bug in cropping gradient

--- caffe2/python/operator_test/pad_test.py
+++ caffe2/python/operator_test/pad_test.py
@@ -22,25 +22,33 @@ import hypothesis.strategies as st
 import unittest
 import caffe2.python.hypothesis_test_util as hu
 from caffe2.python import core
-from hypothesis import given
+from caffe2.proto import caffe2_pb2
+from hypothesis import assume, given
 
 
 class TestPad(hu.HypothesisTestCase):
-    @given(pad_t=st.integers(-5, 0),
-           pad_l=st.integers(-5, 0),
-           pad_b=st.integers(-5, 0),
-           pad_r=st.integers(-5, 0),
+    @given(pad_t=st.integers(-2, 0),
+           pad_l=st.integers(-2, 0),
+           pad_b=st.integers(-2, 0),
+           pad_r=st.integers(-2, 0),
            mode=st.sampled_from(["constant", "reflect", "edge"]),
-           size_w=st.integers(16, 128),
-           size_h=st.integers(16, 128),
+           size_w=st.integers(5, 8),
+           size_h=st.integers(5, 8),
            size_c=st.integers(1, 4),
            size_n=st.integers(1, 4),
+           dtype=st.sampled_from([np.float32, np.float16]),
            **hu.gcs)
     def test_crop(self,
                   pad_t, pad_l, pad_b, pad_r,
                   mode,
                   size_w, size_h, size_c, size_n,
+                  dtype,
                   gc, dc):
+        if dtype == np.float16:
+            # fp16 is only supported with CUDA
+            assume(gc.device_type == caffe2_pb2.CUDA)
+            dc = [d for d in dc if d.device_type == caffe2_pb2.CUDA]
+
         op = core.CreateOperator(
             "PadImage",
             ["X"],
@@ -49,15 +57,61 @@ class TestPad(hu.HypothesisTestCase):
             pad_l=pad_l,
             pad_b=pad_b,
             pad_r=pad_r,
+            mode=mode,
         )
         X = np.random.rand(
-            size_n, size_c, size_h, size_w).astype(np.float32)
+            size_n, size_c, size_h, size_w).astype(dtype)
 
         def ref(X):
             return (X[:, :, -pad_t:pad_b or None, -pad_l:pad_r or None],)
 
         self.assertReferenceChecks(gc, op, [X], ref)
         self.assertDeviceChecks(dc, op, [X], [0])
+        if dtype != np.float16:  # not ready yet
+            self.assertGradientChecks(gc, op, [X], 0, [0])
+
+    @given(pad_t=st.integers(0, 4),
+           pad_l=st.integers(0, 4),
+           pad_b=st.integers(0, 4),
+           pad_r=st.integers(0, 4),
+           mode=st.sampled_from(["constant", "reflect", "edge"]),
+           size_w=st.integers(5, 8),
+           size_h=st.integers(5, 8),
+           size_c=st.integers(1, 4),
+           size_n=st.integers(1, 4),
+           dtype=st.sampled_from([np.float32, np.float16]),
+           **hu.gcs)
+    def test_pads(self,
+                  pad_t, pad_l, pad_b, pad_r,
+                  mode,
+                  size_w, size_h, size_c, size_n,
+                  dtype,
+                  gc, dc):
+        if dtype == np.float16:
+            # fp16 is only supported with CUDA
+            assume(gc.device_type == caffe2_pb2.CUDA)
+            dc = [d for d in dc if d.device_type == caffe2_pb2.CUDA]
+
+        op = core.CreateOperator(
+            "PadImage",
+            ["X"],
+            ["Y"],
+            pad_t=pad_t,
+            pad_l=pad_l,
+            pad_b=pad_b,
+            pad_r=pad_r,
+            mode=mode,
+        )
+        X = np.random.rand(
+            size_n, size_c, size_h, size_w).astype(dtype)
+
+        def ref(X):
+            return (np.pad(X, [(0, 0), (0, 0), (pad_t, pad_b), (pad_l, pad_r)], mode=mode),)
+
+        self.assertReferenceChecks(gc, op, [X], ref)
+        self.assertDeviceChecks(dc, op, [X], [0])
+        if dtype != np.float16:  # not ready yet
+            self.assertGradientChecks(gc, op, [X], 0, [0])
 
 
 if __name__ == "__main__":
