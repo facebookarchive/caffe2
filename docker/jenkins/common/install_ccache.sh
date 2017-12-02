2b74e1d0432d5b1d1720391b2c0b168cb67b6600
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-12-02 06:01:40 +0000

    Use same ccache branch as before (still without docs)

--- docker/jenkins/common/install_ccache.sh
+++ docker/jenkins/common/install_ccache.sh
@@ -11,7 +11,7 @@ rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 # Also pulls in a commit that disables documentation generation,
 # as this requires asciidoc to be installed (which pulls in a LOT of deps).
 pushd /tmp
-git clone https://github.com/pietern/ccache -b nodoc
+git clone https://github.com/pietern/ccache -b ccbin
 pushd ccache
 ./autogen.sh
 ./configure --prefix=/usr/local
