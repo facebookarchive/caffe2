0e2c65e947268ccfa2c0d3cf0dc306bde0def526
Jesse Hellemn <hellemn@fb.com>
2018-02-14 09:28:48 -0800

    Actually just going to fix othe rmac builds first

---
+++ conda/cuda_full/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Install script for Anaconda environments with CUDA on linux
+# This script is not supposed to be called directly, but should be run by:
+#
+# $ cd <path to caffe2, e.g. ~/caffe2>
+# $ conda build conda/cuda_full
+#
+# If you're debugging this, it may be useful to use the env that conda build is
+# using:
+# $ cd <anaconda_root>/conda-bld/caffe2_<timestamp>
+# $ source activate _h_env_... # some long path with lots of placeholders
+#
+# Also, failed builds will accumulate those caffe2_<timestamp> directories. You
+# can remove them after a succesfull build with
+# $ conda build purge
+#
+
+set -ex
+
+echo "Installing caffe2 to ${PREFIX}"
+
+PYTHON_ARGS="$(python ./scripts/get_python_cmake_flags.py)"
+
+# Build with a big suite of libraries
+CMAKE_ARGS=()
+CMAKE_ARGS+=("-DUSE_CUDA=ON")
+CMAKE_ARGS+=("-DUSE_GFLAGS=ON")
+CMAKE_ARGS+=("-DUSE_GLOG=ON")
+CMAKE_ARGS+=("-DUSE_GLOO=ON")
+CMAKE_ARGS+=("-DUSE_LEVELDB=ON")
+CMAKE_ARGS+=("-DUSE_MPI=ON")
+CMAKE_ARGS+=("-DUSE_NCCL=ON")
+CMAKE_ARGS+=("-DUSE_OPENCV=ON")
+CMAKE_ARGS+=("-DUSE_REDIS=ON")
+CMAKE_ARGS+=("-DBUILD_TEST=OFF")
+
+# Install under specified prefix
+CMAKE_ARGS+=("-DCMAKE_INSTALL_PREFIX=$PREFIX")
+CMAKE_ARGS+=("-DCMAKE_PREFIX_PATH=$PREFIX")
+
+mkdir -p build
+cd build
+cmake "${CMAKE_ARGS[@]}"  $CONDA_CMAKE_ARGS $PYTHON_ARGS ..
+make VERBOSE=1 "-j$(nproc)"
+
+make install/fast
---
+++ docker/jenkins/common/install_nccl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+[ -n "$UBUNTU_VERSION" ]
+
+# This doesn't actually use any NCCL version yet, it's just hardcoded to
+# 2.1 for now
+
+# The nvidia website has separate packages for CUDA 9.0 and 8.0, but they
+# appear to point to the same URL, so we ignore this difference for now
+
+# There are only NCCL packages for Ubuntu 16.04 and 14.04
+if [[ "$UBUNTU_VERSION" == 16.04 ]]; then
+  NCCL_UBUNTU_VER=ubuntu1604
+  NCCL_DEB='nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb'
+elif [[ "$UBUNTU_VERSION" == 14.04 ]]; then
+  NCCL_UBUNTU_VER=ubuntu1404
+  NCCL_DEB='nvidia-machine-learning-repo-ubuntu1404_4.0-2_amd64.deb'
+fi
+
+curl -L0 "http://developer.download.nvidia.com/compute/machine-learning/repos/${NCCL_UBUNTU_VER}/x86_64/${NCCL_DEB}"
+dpkg -i "${NCCL_DEB}"
+apt update
+apt install libnccl2 libnccl-dev
