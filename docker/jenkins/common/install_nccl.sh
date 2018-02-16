deef17108d2554018ad4bc5ee229396027248e98
Jesse Hellemn <hellemn@fb.com>
2018-02-16 14:40:38 -0800

    Making NCCL installation specific to CUDA version

--- docker/jenkins/common/install_nccl.sh
+++ docker/jenkins/common/install_nccl.sh
@@ -3,12 +3,10 @@
 set -ex
 
 [ -n "$UBUNTU_VERSION" ]
+[ -n "$CUDA_VERSION" ]
 
 # This doesn't actually use any NCCL version yet, it's just hardcoded to
 # 2.1 for now
-
-# The nvidia website has separate packages for CUDA 9.0 and 8.0, but they
-# appear to point to the same URL, so we ignore this difference for now
 
 # There are only NCCL packages for Ubuntu 16.04 and 14.04
 if [[ "$UBUNTU_VERSION" == 16.04 ]]; then
@@ -23,8 +21,12 @@ else
 fi
 
 if [ -n "$NCCL_UBUNTU_VER" ]; then
+  # The deb is agnostic of CUDA version
   curl -LO "http://developer.download.nvidia.com/compute/machine-learning/repos/${NCCL_UBUNTU_VER}/x86_64/${NCCL_DEB}"
   dpkg -i "${NCCL_DEB}"
+
+  # Actually installing takes into account CUDA version
+  NCCL_LIB_VERSION="2.1.4-1+cuda${CUDA_VERSION}"
   apt update
-  apt install libnccl2 libnccl-dev
+  apt install libnccl2=$NCCL_LIB_VERSION libnccl-dev=NCCL_LIB_VERSION
 fi
