c6f40a8163c620f2ee5cc908ad28bd8056585b3d
Jesse Hellemn <hellemn@fb.com>
2018-02-27 10:32:54 -0800

    Trying fuller cuda_cull

--- conda/cuda_full/build.sh
+++ conda/cuda_full/build.sh
@@ -28,15 +28,16 @@ CMAKE_ARGS+=("-DUSE_CUDA=ON")
 CMAKE_ARGS+=("-DUSE_GFLAGS=ON")
 CMAKE_ARGS+=("-DUSE_GLOG=ON")
 CMAKE_ARGS+=("-DUSE_GLOO=ON")
+CMAKE_ARGS+=("-DUSE_LEVELDB=ON")
 CMAKE_ARGS+=("-DUSE_LMBD=ON")
 CMAKE_ARGS+=("-DUSE_NCCL=ON")
 CMAKE_ARGS+=("-DUSE_OPENCV=ON")
+CMAKE_ARGS+=("-DUSE_OPENMP=ON")
 CMAKE_ARGS+=("-DUSE_REDIS=ON")
+CMAKE_ARGS+=("-DUSE_ROCKSDB=ON")
 
 # Explicitly turn unused packages off to prevent cmake from trying to find
 # system libraries
-CMAKE_ARGS+=("-DUSE_LEVELDB=OFF")
-CMAKE_ARGS+=("-DUSE_ROCKSDB=OFF")
 
 # Install under specified prefix
 CMAKE_ARGS+=("-DCMAKE_INSTALL_PREFIX=$PREFIX")
