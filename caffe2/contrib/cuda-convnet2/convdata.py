8d85e707852e47e2a31640b34efcac36377ee042
Xiaomeng Yang <yangxm@devgpu149.prn2.facebook.com>
2018-02-27 17:25:55 -0800

    Update py files for build

--- caffe2/contrib/cuda-convnet2/convdata.py
+++ caffe2/contrib/cuda-convnet2/convdata.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from python_util.data import *
 import numpy.random as nr
 import numpy as n
