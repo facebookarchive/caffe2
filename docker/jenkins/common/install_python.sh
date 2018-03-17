7265e649cf8a3f62ea52346cdec43800cf675326
pjh5 <hellemn@fb.com>
2018-03-17 09:32:18 -0700

    Preventing installing two versions of setuptools

--- docker/jenkins/common/install_python.sh
+++ docker/jenkins/common/install_python.sh
@@ -109,6 +109,11 @@ if [ -n "${INSTALL_SETUPTOOLS}" ]; then
   "$PYTHON" setup.py install
   popd
   rm -rf setuptools-38.5.1*
+else
+  # Upgrade setuptools
+  # setuptools 38.5.2 seems to be buggy, see error in
+  # https://ci.pytorch.org/jenkins/job/caffe2-docker/job/py3.6-gcc5-ubuntu16.04/35/consoleFull
+  pip install -U pip setuptools!=38.5.2
 fi
 
 # Install pip from source.
@@ -121,11 +126,6 @@ pushd pip-9.0.1
 "$PYTHON" setup.py install
 popd
 rm -rf pip-9.0.1*
-
-# Upgrade setuptools
-# setuptools 38.5.2 seems to be buggy, see error in
-# https://ci.pytorch.org/jenkins/job/caffe2-docker/job/py3.6-gcc5-ubuntu16.04/35/consoleFull
-pip install -U pip setuptools!=38.5.2
 
 # tornado 5.0 requires Python 2.7.9+ or 3.4+
 if [[ $($PYTHON -c 'import sys; print(int(sys.version_info <= (2, 7, 9) or sys.version_info <= (3, 4)))' == 1) ]]; then
