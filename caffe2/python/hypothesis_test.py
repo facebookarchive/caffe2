e7de9bfec92840db7c38927ac0181c18306ccc3a
Matan Appelbaum <mappelbaum@fb.com>
2018-03-20 11:39:49 -0700

    Exported AtomicIterOp count

--- caffe2/python/hypothesis_test.py
+++ caffe2/python/hypothesis_test.py
@@ -1697,18 +1697,19 @@ class TestOperators(hu.HypothesisTestCase):
             np.asarray([initial_iters]).astype(np.int64))
         concurrent_steps = core.ExecutionStep("concurrent_steps",
                                               num_iter=num_iters)
-        sre_ops = []
         for i in range(num_nets):
             net = core.Net("net_{}".format(i))
+            # clear stats from global stats registry
+            net.StatRegistryExport(
+                [], ["_k{}".format(i), "_v{}".format(i), "_t{}".format(i)])
             net.AtomicIter([iter_mutex, "iter"], ["iter"])
             step = core.ExecutionStep("step", [net])
-            sre_op = net.StatRegistryExport(
+            net.StatRegistryExport(
                 [], [
                     "stat_key_{}".format(i), "stat_value_{}".format(i),
                     "stat_ts_{}".format(i)
                 ]
             )
-            sre_ops.append(sre_op)
             concurrent_steps.AddSubstep(step)
 
         concurrent_steps.SetConcurrentSubsteps(True)
@@ -1726,7 +1727,8 @@ class TestOperators(hu.HypothesisTestCase):
             self.assertEqual(b'atomic_iter/stats/iter/num_iter', stat_key[0])
             stat_value = self.ws.blobs[("stat_value_{}".format(i))].fetch()
             self.assertEqual(np.int64, stat_value.dtype)
-            self.assertEqual(1, stat_value[0])
+            if num_iters > 0:
+                self.assertEqual(1, stat_value[0])
 
     @given(a=hu.tensor(),
            src=st.sampled_from(list(viewkeys(_NUMPY_TYPE_TO_ENUM))),
